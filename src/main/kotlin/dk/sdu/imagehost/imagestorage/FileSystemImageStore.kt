4d562563cc0ba4e384cc4da795415577edf33ac5
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-11-12 21:27:04 +0100

    Add Test cases

--- src/main/kotlin/dk/sdu/imagehost/imagestorage/FileSystemImageStore.kt
+++ src/main/kotlin/dk/sdu/imagehost/imagestorage/FileSystemImageStore.kt
@@ -3,23 +3,34 @@ package dk.sdu.imagehost.imagestorage
 import java.io.File
 
 class FileSystemImageStore(folderName: String) {
-    val folder = File(".", folderName).also {
+    private val folder = File(".", folderName).also {
         it.mkdirs()
     }
-    val extension = ".png"
+    private val extension = "png"
+    private val ending = ".$extension"
 
     fun listFiles(): List<String> =
         folder.listFiles()!!.asSequence().filter {
             it.extension.equals(extension, ignoreCase = true)
         }.map { it.nameWithoutExtension }.toList()
 
-    fun save(data: ByteArray, name: String){
-        val file = File(folder, name + extension)
+    fun save(data: ByteArray, name: String) {
+        val file = File(folder, name + ending)
         file.writeBytes(data)
     }
 
+    fun delete(name: String) {
+        val file = File(folder, name + ending)
+        file.delete()
+    }
+
+    fun exists(name: String): Boolean {
+        val file = File(folder, name + ending)
+        return file.exists()
+    }
+
     fun load(name: String): ByteArray {
-        val file = File(folder, name + extension)
+        val file = File(folder, name + ending)
         return file.readBytes()
     }
 }
--- src/test/kotlin/FileSystemImageStoreTest.kt
+++ src/test/kotlin/FileSystemImageStoreTest.kt
@@ -1,18 +1,29 @@
 import dk.sdu.imagehost.imagestorage.FileSystemImageStore
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
 import java.io.File
+import java.io.FileNotFoundException
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 object FileSystemImageStoreTest {
 
     val folderName = "testStore"
+    val folder = File(".", folderName)
     lateinit var imageStore: FileSystemImageStore
+    lateinit var lighthouse: ByteArray
+    lateinit var pancakes: ByteArray
+    lateinit var rocket: ByteArray
+
     @BeforeAll
     fun setup(){
         File(".", folderName).deleteRecursively()
+
+        val classloader = FileSystemImageStoreTest::class.java.classLoader
+        fun loadResource(string: String) = classloader.getResource(string)!!.readBytes()
+        lighthouse = loadResource("lighthouse.png")
+        pancakes = loadResource("pancakes.png")
+        rocket = loadResource("rocket.png")
+
         imageStore = FileSystemImageStore(folderName)
     }
 
@@ -21,8 +32,68 @@ object FileSystemImageStoreTest {
         assert(imageStore.listFiles().isEmpty())
     }
 
+    @Test
+    fun `an image is saved`(){
+        imageStore.save(lighthouse, "lighthouse")
+        val file = File(folder, "lighthouse.png")
+        assertTrue(imageStore.exists("lighthouse"))
+        assertTrue(file.exists())
+        assertTrue(file.totalSpace >= lighthouse.size)
+    }
+
+    @Test
+    fun `an image is loaded`(){
+        imageStore.save(pancakes, "pancakes")
+        val file = File(folder, "pancakes.png")
+        assertTrue(imageStore.exists("pancakes"))
+        assertTrue(file.exists())
+        assertTrue(file.totalSpace >= lighthouse.size)
+        val readout = imageStore.load("pancakes")
+        assertEquals(pancakes.size, readout.size)
+        assertArrayEquals(pancakes, readout)
+    }
+
+    @Test
+    fun `an image is overwritten`(){
+        imageStore.save(pancakes, "img")
+        assertTrue(imageStore.exists("img"))
+        imageStore.save(lighthouse, "img")
+        assertTrue(imageStore.exists("img"))
+        val img = imageStore.load("img")
+        assertArrayEquals(lighthouse, img)
+    }
+
+    @Test
+    fun `reading a nonexistent image produces an error`(){
+        assertFalse(imageStore.exists("rumplestiltkin"))
+        assertThrows<FileNotFoundException> { imageStore.load("rumplestiltkin") }
+    }
+
+    @Test
+    fun `an image is deleted`(){
+        imageStore.save(pancakes, "pancakes")
+        assertTrue(imageStore.exists("pancakes"))
+        imageStore.delete("pancakes")
+        assertFalse(imageStore.exists("pancakes"))
+    }
+
+    @Test
+    fun `there are three images saved`(){
+        imageStore.save(pancakes, "pancakes")
+        imageStore.save(lighthouse, "lighthouse")
+        imageStore.save(rocket, "rocket")
+        val images = imageStore.listFiles().toSet()
+        assertEquals(3, images.size)
+        assertEquals(setOf("pancakes", "lighthouse", "rocket"), images)
+    }
+
+    @AfterEach
+    fun cleanupFiles(){
+        File(".", folderName).listFiles()?.forEach { it.delete() }
+    }
+
     @AfterAll
-    fun cleanup(){
+    fun cleanupFolder(){
         File(".", folderName).deleteRecursively()
     }
 
