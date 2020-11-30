2d8e0ad888ad147006c32fddc75ddc401d7f71b2
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-11-30 21:49:53 +0100

    Add JSON parsing

--- src/main/kotlin/dk/sdu/imagehost/imagestorage/ampq/AMPQ.kt
+++ src/main/kotlin/dk/sdu/imagehost/imagestorage/ampq/AMPQ.kt
@@ -1,6 +1,10 @@
 package dk.sdu.imagehost.imagestorage.ampq
 
+import com.beust.klaxon.Klaxon
 import com.rabbitmq.client.*
+import dk.sdu.imagehost.imagestorage.json.Base64Converter
+import dk.sdu.imagehost.imagestorage.json.DateTimeConverter
+import dk.sdu.imagehost.imagestorage.json.UUIDConverter
 import java.io.Closeable
 import java.net.URI
 
@@ -15,6 +19,8 @@ class AMPQ(val uri: URI, val callback: EventCallback) : Closeable {
         exchangeDeclare("rapid", "direct")
         queueDeclare("ImageStorage", false, false, false, emptyMap())
     }
+
+    val klaxon = Klaxon().converter(Base64Converter).converter(DateTimeConverter).converter(UUIDConverter)
 
     init {
         subscribe("ImageCreateRequest", DeliverCallback(::requestCreate))
@@ -36,23 +42,23 @@ class AMPQ(val uri: URI, val callback: EventCallback) : Closeable {
     }
 
     fun requestCreate(consumerTag: String, delivery: Delivery) {
-        //TODO: Parse JSON from request body
-        //TODO: Call callback
+        val event = klaxon.parse<ImageStorageEvent.Request.Create>(String(delivery.body))!!
+        callback(event, ::send)
     }
 
     fun requestLoad(consumerTag: String, delivery: Delivery) {
-        //TODO: Parse JSON from request body
-        //TODO: Call callback
+        val event = klaxon.parse<ImageStorageEvent.Request.Load>(String(delivery.body))!!
+        callback(event, ::send)
     }
 
     fun requestDelete(consumerTag: String, delivery: Delivery) {
-        //TODO: Parse JSON from request body
-        //TODO: Call callback
+        val event = klaxon.parse<ImageStorageEvent.Request.Delete>(String(delivery.body))!!
+        callback(event, ::send)
     }
 
     @Suppress("UNREACHABLE_CODE")
     fun send(response: ImageStorageEvent.Response) {
-        val json: String = TODO("Convert response to JSON")
+        val json: String = klaxon.toJsonString(response)
         responseChannel.basicPublish("rapid", response.TAG, null, json.toByteArray());
     }
 
