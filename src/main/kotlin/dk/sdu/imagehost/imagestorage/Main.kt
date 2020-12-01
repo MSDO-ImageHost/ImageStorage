178dc05eab74584427904c43244e376251b5c292
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-12-01 15:24:58 +0100

    Delete unused code Sorry, Jakob :-(

--- src/main/kotlin/dk/sdu/imagehost/imagestorage/Main.kt
+++ src/main/kotlin/dk/sdu/imagehost/imagestorage/Main.kt
@@ -1,12 +1,8 @@
 package dk.sdu.imagehost.imagestorage
 
 import dk.sdu.imagehost.imagestorage.ampq.AMQP
-import dk.sdu.imagehost.imagestorage.ampq.EventCallback
-import dk.sdu.imagehost.imagestorage.ampq.ImageStorageEvent
 import dk.sdu.imagehost.imagestorage.db.Parameters
 import java.net.URI
-import java.time.LocalDateTime
-import java.util.*
 
 fun main() {
     val service = ImageStorageService(Parameters.env(), "img")
