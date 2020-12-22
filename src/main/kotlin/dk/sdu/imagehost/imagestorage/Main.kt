bf2469319f9d074b50a1b1dc7bdf9d71b47c440c
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-12-22 12:21:54 +0100

    Enable an environment variable "IMG_DIR" for the folder where images are stored

--- src/main/kotlin/dk/sdu/imagehost/imagestorage/Main.kt
+++ src/main/kotlin/dk/sdu/imagehost/imagestorage/Main.kt
@@ -4,7 +4,7 @@ import dk.sdu.imagehost.imagestorage.ampq.AMQP
 import dk.sdu.imagehost.imagestorage.db.Parameters
 
 fun main() {
-    val service = ImageStorageService(Parameters.env(), "img")
+    val service = ImageStorageService(Parameters.env(), System.getenv("IMG_DIR") ?: "img")
 
     val callback = EventHandler(service)
     val ampq = AMQP(AMQP.Parameters.env(), callback)
